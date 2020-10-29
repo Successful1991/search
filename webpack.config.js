b3ccacfa48d7f8d03ec1a0e0f2785e4b88affda8
Artem Tsys <19successful91@gmail.com>
2020-10-29 18:52:59 +0300

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,49 @@
+const webpack = require('webpack');
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
+
+const config = {
+    entry: {
+        view: "./src/assets/scripts/index-app.js",
+    },
+    output: {
+        filename: "[name].bundle.js"
+    },
+    module: {
+        rules: [{
+            test: /\.js$/,
+            exclude: /node_modules/,
+            use: {
+                loader: 'babel-loader',
+                options: {
+                    presets: ['@babel/preset-env']
+                }
+            }
+        },{
+            test: /\.(png|jpe?g|gif)$/i,
+            loader: 'file-loader',
+            options: {
+                outputPath: '../images',
+                publicPath: 'assets/s3d/images',
+            },
+        }
+        ]
+    },
+    optimization: {
+        splitChunks: {
+            cacheGroups: {
+                commons: {
+                    test: /[\\/]node_modules[\\/]/,
+                    name: 'vendors',
+                    chunks: 'all'
+                }
+            }
+        }
+    },
+    plugins: [
+        new UglifyJSPlugin({
+            sourceMap: true
+        })
+    ]
+};
+
+module.exports = config;
